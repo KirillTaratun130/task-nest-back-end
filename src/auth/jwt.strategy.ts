ed4aa0a198fdb803acba99e48a23ffa7cebec2e2
Kirill Taratun <taratunse@gmail.com>
2026-02-11 13:14:12 +0700

    интеграция JWT стратегии

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from "../user/user.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private configService: ConfigService, private userService: UserService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.get('JWT_SECRET')
+        });
+    }
+    async validate({ id }: {id: string}) {
+        const user = this.userService.getById(id);
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        return user;
+    }
+}
